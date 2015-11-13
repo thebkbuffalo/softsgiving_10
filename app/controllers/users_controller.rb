115c0909f19b2c68ed036ecb51bc508771baefc8
Evan Berg <evan.m.berg@gmail.com>
2015-11-13 12:44:52 -0500

    added all things for paperclip...i think

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:dish, :number_of_guests, :note)
+      params.require(:user).permit(:dish, :number_of_guests, :note, :name, :avatar)
     end
 end
