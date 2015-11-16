34b86d84f929b1b426504c2dd6f8ef510226845c
Evan Berg <evan.m.berg@gmail.com>
2015-11-16 16:36:33 -0500

    ordered renderings, gotta add count

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.all.order('created_at desc')
   end
 
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+
+  def users_count(users)
+    users
+  end
 end
