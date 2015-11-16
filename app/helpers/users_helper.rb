7afbe0806ff56e98cd68267df1f4a8ba92da021b
Evan Berg <evan.m.berg@gmail.com>
2015-11-16 17:50:06 -0500

    added some logic to get the total count

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
 
-  def users_count(users)
-    users
+  def users_count
+    @all_users = User.all
+    total_users = @all_users.length
+    total_guests = @all_users.map { |user| user.number_of_guests }.sum
+    return total_guests + total_users
   end
+
 end
