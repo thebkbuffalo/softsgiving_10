9d439bac37632ad1d3882aac7a07e1c7f0fd7e0e
Evan Berg <evan.m.berg@gmail.com>
2015-11-16 18:08:19 -0500

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "150x150>" }, default_url: ""
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
