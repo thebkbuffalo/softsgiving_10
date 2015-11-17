598cc2a5d218d34b80b5aba0ca65401c76aa9154
Evan Berg <evan.m.berg@gmail.com>
2015-11-17 13:14:31 -0500

    fixed stupid error with nil value in array, cause im dumb and forgot to colapse it

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   def users_count
     @all_users = User.all
     total_users = @all_users.length
-    total_guests = @all_users.map { |user| user.number_of_guests }.sum
+    total_guests = @all_users.map { |user| user.number_of_guests }.compact.sum
     return total_guests + total_users
   end
 
