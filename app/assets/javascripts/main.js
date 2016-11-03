668c0f7c01f75097378c318f62b22d2077efaeb2
Evan Berg <evan.m.berg@gmail.com>
2016-11-02 20:29:50 -0700

    lookin better

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   $('.clickable').on('click', function(){
     var info = $(this).find('.more_info');
     if(info.css('display') == 'none'){
-      info.slideDown('medium', function(){});
+      info.slideDown('fast', function(){});
     }else{
-      info.slideUp('medium', function(){});
+      info.slideUp('fast', function(){});
     }
   });
 });
